b74cc5714390743aa7c181a2ca405c0cd1c6bc00
Chad Granum <exodist7@gmail.com>
2012-10-22 05:10:17 +0000

    Import of EXODIST/Declare-Opts-0.001 from CPAN.
    gitpan-cpan-distribution: Declare-Opts
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         EXODIST/Declare-Opts-0.001.tar.gz
    gitpan-cpan-author:       EXODIST
    gitpan-cpan-maturity:     released

---
+++ lib/Declare/CLI.pm
@@ -0,0 +1,21 @@
+package Declare::CLI;
+use strict;
+use warnings;
+
+our $VERSION = 0.001;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Declare::CLI - Declarative CLI definition.
+
+=head1 DESCRIPTION
+
+This will tie together L<Declare::Opts> and L<Declare::Args>.
+
+=cut
